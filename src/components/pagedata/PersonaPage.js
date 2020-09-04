ea42df5b8ca9be216b127f5116b0f34e16432581
pedroobando <pedroobando@hotmail.com>
2020-09-04 19:59:46 -0400

    presona html

--- src/components/pagedata/PersonaPage.js
+++ src/components/pagedata/PersonaPage.js
@@ -1,20 +1,40 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
 
 export const PersonaPage = () => {
-  const history = useHistory();
-  const handleBack = (e) => {
-    e.preventDefault();
-    history.replace('/datos');
-  };
-
   return (
-    <div>
-      <h1>Pagina Persona</h1>
-      <hr />
-      <p>
-        <Link onClick={handleBack}>Volver</Link>
-      </p>
+    <div className="container-fluid">
+      <div className="row">
+        <aside className="col-2 px-1 position-fixed" id="sticky-sidebar">
+          <button className="btn btn-outline-primary btn-block">Nueva Persona</button>
+        </aside>
+
+        <main className="col offset-2" id="main">
+          <h3>Persona</h3>
+          <hr />
+          molestiae laudantium! Saepe architecto ipsa debitis ex nobis ab tempore quisquam
+          libero eligendi! Ipsa quas aliquid mollitia odit temporibus eaque suscipit
+          tempore accusamus nam repudiandae asperiores laboriosam, saepe veniam,
+          accusantium maxime. Fuga eum sint hic veniam aliquam sequi cupiditate et?
+          Temporibus inventore fugit, itaque illum optio quisquam in vero. Voluptas
+          officiis modi consequatur voluptatibus natus qui eum omnis ad. Voluptas alias
+          temporibus odit vitae architecto, culpa eligendi facilis repellat inventore?
+          Libero necessitatibus assumenda voluptate voluptatem! Reprehenderit quas odit
+          nisi minima, eveniet, dicta ea dolorum assumenda cum veniam velit distinctio
+          animi? Maiores, nihil rerum iste ipsa praesentium ratione assumenda mollitia
+          vel! Iure animi sequi voluptatum minima placeat, necessitatibus dolorem sapiente
+          delectus molestias illo impedit accusantium, id quae maxime dolores vero ab!
+          Molestias ipsa quidem reiciendis illo atque veniam natus repellat obcaecati.
+          Iste quisquam, nam iure consequatur minus iusto quia ullam ea velit eveniet
+          obcaecati dolorum unde dignissimos provident distinctio fugiat expedita tempora,
+          repudiandae ratione! Obcaecati velit reprehenderit, quidem sapiente qui aut.
+          Quaerat est repellat quia vitae tenetur placeat eveniet facilis optio iusto
+          excepturi sequi dolores reprehenderit animi explicabo provident, quibusdam ea?
+          Quia consequatur minima sapiente sunt illo amet officiis ex odit? Fuga provident
+          id esse consequuntur eligendi, quis, eos aperiam molestiae cupiditate,
+          repudiandae dolorum quas suscipit blanditiis quisquam deleniti! Facere vel fugit
+          ratione ut suscipit et excepturi repudiandae. Eaque, dicta perferendis?
+        </main>
+      </div>
     </div>
   );
 };
